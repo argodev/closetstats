8a05e6533add4f7a9f1d99dd7ea76cf408889773
Gillen <ru7@ornl.gov>
2019-08-18 16:30:24 -0400

    Working on bettering things

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,22 @@ function enablePickup() {
     $("#dropoffForm").show();
     $("#pickupForm").hide();
 }
+
+
+// https://www.sitepoint.com/url-parameters-jquery/
+//URL is http://www.example.com/mypage?ref=registration&email=bobo@example.com
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+                      .exec(window.location.search);
+
+    return (results !== null) ? results[1] || 0 : false;
+}
+
+
+function selectLocation() {
+    var location = $.urlParam('loc');
+
+    if ((location) && (location === '2')) {
+        $("#closetLocation").val('Oak Ridge');
+    }
+}
