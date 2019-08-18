b5dbc99dc377af0f1cc0cf02c3be043dba465108
Gillen <ru7@ornl.gov>
2019-08-18 16:02:18 -0400

    testing html

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function enableDropOff() {
+    $("#pickupForm").show();
+    $("#dropoffForm").hide();
+}
+
+function enablePickup() {
+    $("#dropoffForm").show();
+    $("#pickupForm").hide();
+}
