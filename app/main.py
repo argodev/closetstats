1535712c7ce2e651c665628a315e1e324ba4b923
Rob Gillen <argodev@gmail.com>
2019-09-30 20:47:13 -0400

    cleaning up page a little

--- app/main.py
+++ app/main.py
@@ -19,10 +19,9 @@ db_name = 'closet_stats'
 db_url = os.getenv('CSTATS_DATABASE_SERVER', 'example.com')
 db_user = os.getenv('CSTATS_DATABASE_USER', 'user')
 db_pass = os.getenv('CSTATS_DATABASE_PWD', 'pass')
-mongo_conn_str = os.getenv('MONGODB_URI', 'notset')
 
 @app.route('/api/visit', methods=["POST"])
-def stripeTest():
+def postData():
     if request.method == "POST":
         try:
             json_dict = request.get_json()
@@ -31,8 +30,7 @@ def stripeTest():
 
             # we don't have a high volume, so we open/close on each request
             myclient = pymongo.MongoClient("mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority" % (db_user, db_pass, db_url, db_name))
-            #myclient = pymongo.MongoClient(mongo_conn_str)
-            mydb = myclient["closet_stats"]
+            mydb = myclient[db_name]
             mycol = mydb["visits"]
             mycol.insert_one(json_dict)
             myclient.close()
